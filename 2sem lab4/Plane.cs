65a005fe6dac0ab195e8d3cc349bffd618e3442c
Katerina <161355341+kkatiefk@users.noreply.github.com>
2024-04-03 15:01:58 +0300

    Add files via upload

---
+++ 2sem lab4/Plane.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Створити об'єкт класу Літак, використовуючи класи Крило, Шасі, Двигун. 
+//Методи: літати, задавати маршрут, вивести на консоль маршрут.
+namespace _2sem_lab4
+{
+    public class Plane
+    {
+        public string Name { get; set; }
+        public Wings Wings { get; set; }
+        public Chassis Chassis { get; set; }
+        public Engine Engine { get; set; }
+
+        public Plane(string name, Wings wings, Chassis chassis, Engine engine)
+        {
+            Name = name;
+            Wings = wings;
+            Chassis = chassis;
+            Engine = engine;
+        }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Wings = {Wings}, Chassis = {Chassis}, Engine = {Engine}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Plane plane)
+                return Name == plane.Name && Wings.Equals(plane.Wings) && Chassis.Equals(plane.Chassis) && Engine.Equals(plane.Engine);
+            return false;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("\nThe plane is taking off!");
+        }
+
+        public void Route(string route)
+        {
+            if (string.IsNullOrEmpty(route))
+            {
+                Console.WriteLine("We don't have a route.");
+            }
+            else
+            {
+                Console.WriteLine("Route: " + route);
+            }
+        }
+    }
+
+    public class Wings
+    {
+        public string Shape { get; set; }
+
+        public override string ToString()
+        {
+            return $"Shape = {Shape}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Wings wing)
+                return Shape == wing.Shape;
+            return false;
+        }
+    }
+
+    public class Chassis
+    {
+        public bool IsPresent { get; set; }
+
+        public override string ToString()
+        {
+            return $"IsPresent = {IsPresent}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Chassis chassis)
+                return IsPresent == chassis.IsPresent;
+            return false;
+        }
+    }
+
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public string Model { get; set; }
+
+        public override string ToString()
+        {
+            return $"HorsePower = {HorsePower}, Model = {Model}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Engine engine)
+                return HorsePower == engine.HorsePower && Model == engine.Model;
+            return false;
+        }
+    }
+}
---
+++ 2sem lab4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Створити об'єкт класу Літак, використовуючи класи Крило, Шасі, Двигун. 
+//Методи: літати, задавати маршрут, вивести на консоль маршрут.
+using _2sem_lab4;
+
+Wings wings = new Wings
+{
+    Shape = "Long wings"
+};
+Console.WriteLine("Wings: " + wings);
+
+Chassis chassis = new Chassis
+{
+    IsPresent = true
+};
+Console.WriteLine("Chassis: " + chassis);
+
+Engine engine = new Engine
+{
+    HorsePower = 5000,
+    Model = "WRJ1990"
+};
+Console.WriteLine("Engine: " + engine);
+
+Plane plane = new Plane("Boeing 777", wings, chassis, engine);
+Console.WriteLine("Plane: " + plane);
+
+plane.Fly();
+
+Console.WriteLine();
+
+plane.Route("Kyiv - New York");
+
+Console.ReadKey();
